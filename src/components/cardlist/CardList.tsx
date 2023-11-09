978da8c0c94c95d93e34b00129e4018ee09d26fb
nauvalyusufaddairy <nauvalyusufad@gmail.com>
2023-11-10 05:43:13 +0700

    still error

--- src/components/cardlist/CardList.tsx
+++ src/components/cardlist/CardList.tsx
@@ -4,18 +4,18 @@ import styles from "./cardlist.module.css";
 import Image from "next/image";
 
 export default async function Cardlist({ page }: { page: number }) {
-  const getData = async () => {
-    const res = await fetch(`http://localhost:3000/api/posts?page=${page}`, {
-      cache: "no-store",
-    });
+  // const getData = async () => {
+  //   const res = await fetch(`http://localhost:3000/api/posts?page=${page}`, {
+  //     cache: "no-store",
+  //   });
 
-    if (!res.ok) {
-      throw new Error("Failed");
-    }
+  //   if (!res.ok) {
+  //     throw new Error("Failed");
+  //   }
 
-    return res.json();
-  };
-  const data = await getData();
+  //   return res.json();
+  // };
+  // const data = await getData();
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Post</h1>
