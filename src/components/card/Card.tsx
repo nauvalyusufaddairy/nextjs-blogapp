2c2b82e8f3e7459bdc48341dc457ede320ca26ad
nauvalyusufaddairy <nauvalyusufad@gmail.com>
2023-11-17 09:29:57 +0700

    evert "3:00:00"
    This reverts commit 3fe428a532c261f69a7ec7716dd948d5a9904d1e.
    
    	modified:   src/components/card/Card.tsx
    	modified:   src/components/cardlist/CardList.ts

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,22 +2,27 @@ import Link from "next/link";
 import styles from "./card.module.css";
 import Image from "next/image";
 
-export default function Card({ key, item }: { key: string; item: Post }) {
+export default function Card() {
   return (
     <div className={styles.container}>
       <div className={styles.imageContainer}>
-        <Image src={item.img ?? ""} fill alt="" className={styles.image} />
+        <Image src="/p1.jpeg" fill alt="" className={styles.image} />
       </div>
       <div className={styles.textContainer}>
         <div className={styles.detail}>
-          <span className={styles.date}>{item.createdAt}</span>
-          <span className={styles.category}>{item.catSlug}</span>
+          <span className={styles.date}>11.02.2023</span>
+          <span className={styles.category}>culture</span>
         </div>
         <Link href={"/"}>
-          <h1>{item.title}</h1>
+          <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h1>
         </Link>
 
-        <p className={styles.desc}>{item.desc}</p>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti est
+          illum minima delectus autem laboriosam consequuntur tempore repellat
+          hic, ad nesciunt placeat rem neque, consequatur eos cum nostrum
+          repudiandae totam!
+        </p>
         <Link className={styles.link} href={"/"}>
           Read more
         </Link>
--- src/components/cardlist/CardList.tsx
+++ src/components/cardlist/CardList.tsx
@@ -2,27 +2,28 @@ import Card from "../card/Card";
 import Pagination from "../pagination/Pagination";
 import styles from "./cardlist.module.css";
 import Image from "next/image";
-const getData = async (page: number) => {
-  const res = await fetch(`http://localhost:3000/api/posts?page=${page}`, {
-    cache: "no-store",
-  });
 
-  if (!res.ok) {
-    throw new Error("Failed");
-  }
+export default async function Cardlist({ page }: { page: number }) {
+  // const getData = async () => {
+  //   const res = await fetch(`http://localhost:3000/api/posts?page=${page}`, {
+  //     cache: "no-store",
+  //   });
 
-  return res.json();
-};
-export default async function Cardlist({ page }: { page?: number }) {
-  const data: [{}] = await getData(page ?? 1);
-  console.log("cardlist", data);
+  //   if (!res.ok) {
+  //     throw new Error("Failed");
+  //   }
+
+  //   return res.json();
+  // };
+  // const data = await getData();
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Post</h1>
       <div className={styles.posts}>
-        {data.map((item: any) => (
-          <Card key={item._id} item={item} />
-        ))}
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
       <Pagination />
     </div>
